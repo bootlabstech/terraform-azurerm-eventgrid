8621e38a7a9cc64d47209fb7d8e647614b4905f0
Lalitha <lalitha@Lalithas-MacBook-Air.local>
2026-06-23 17:06:49 +0530

    system topic

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "id" {
-  value       = azurerm_eventgrid_topic.eventgrid.id
+  value       = azurerm_eventgrid_system_topic.eventgrid.id
   description = "The output id of the eventgrid created"
 }
